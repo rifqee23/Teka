3ff2da4b3a0f6fc104edb54cf4896b2a57671658
rifqee23 <rifqifebrianto746@gmail.com>
2024-11-11 15:07:15 +0700

    feat: implementation nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,13 @@
-import { Stack } from "expo-router";
+import { Stack, Slot } from "expo-router";
+
+// Import your global CSS file
+import "@/globals.css";
 
 export default function RootLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" />
+      <Slot />
     </Stack>
   );
 }
